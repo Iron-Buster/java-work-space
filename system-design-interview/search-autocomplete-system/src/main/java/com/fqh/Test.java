701fbae8a790cfed1ea4aa6a5e811b5998e2533e
vq <1457480465@qq.com>
2024-02-21 17:37:55 +0800

    😀 system-design-interview: 搜索自动补全系统设计 Trie设计的优化

--- system-design-interview/search-autocomplete-system/src/main/java/com/fqh/Test.java
+++ system-design-interview/search-autocomplete-system/src/main/java/com/fqh/Test.java
@@ -2,7 +2,9 @@ package com.fqh;
 
 import com.fqh.data_structure.Trie;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +17,20 @@ public class Test {
     public static void main(String[] args) {
         Trie trie = new Trie();
         Map<String, Integer> map = new HashMap<>();
+        map.put("be", 15);
+        map.put("bet", 29);
+        map.put("bee", 20);
+        map.put("beer", 10);
+        map.put("best", 35);
+        map.put("buy", 14);
         trie.initTrie(map);
+        String[] res1 = trie.findTopKthWords("b");
+        String[] res2 = trie.findTopKthWords("be");
+        String[] res3 = trie.findTopKthWords("bee");
+        String[] res4 = trie.findTopKthWords("bu");
+        System.out.println(Arrays.toString(res1));
+        System.out.println(Arrays.toString(res2));
+        System.out.println(Arrays.toString(res3));
+        System.out.println(Arrays.toString(res4));
     }
 }
--- system-design-interview/search-autocomplete-system/src/main/java/com/fqh/data_structure/Trie.java
+++ system-design-interview/search-autocomplete-system/src/main/java/com/fqh/data_structure/Trie.java
@@ -6,15 +6,14 @@ package com.fqh.data_structure;
  * @since 2024/2/21 16:28
  **/
 
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * Trie模板
  */
 public class Trie {
     private static final int MAX_KTH = Integer.MAX_VALUE / 2;
+    private static final int K = 5;
     private final Node root;
 
     public Trie() {
@@ -28,24 +27,32 @@ public class Trie {
     public void initTrie(Map<String, Integer> wordMap) {
         System.out.println("------------------ 初始化Trie ------------------");
         wordMap.forEach(this::insert);
+        initKthMostNodeQueue();
         System.out.println("------------------ 初始化完成 ------------------");
     }
 
     /**
      * 通过传入的前缀，返回前k个高频匹配词
      * @param prefix 前缀
-     * @param k      ktH
      * @return
      */
-    public List<String> findTopKthWords(String prefix, int k) {
-        if (k > MAX_KTH) {
-            throw new IllegalArgumentException("k must less than Integer.MAX_VALUE / 2");
+    public String[] findTopKthWords(String prefix) {
+        // limit the prefix length
+        if (prefix.length() > K) {
+            return new String[0];
         }
-        if (k == -1) {
-
+        Node node = search(prefix);
+        if (node == null) {
+            return new String[0];
         }
-
-        return null;
+        PriorityQueue<Node> pq = node.kthMostNodeQueue;
+        int n = pq.size();
+        String[] result = new String[n];
+        // reverse fill the result
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = pq.poll().endWord;
+        }
+        return result;
     }
 
     /**
@@ -68,8 +75,11 @@ public class Trie {
         cur.freq = freq;
     }
 
-
-
+    /**
+     * 搜索一个前缀
+     * @param prefix    前缀
+     * @return
+     */
     private Node search(String prefix) {
         Node cur = root;
         int n = prefix.length();
@@ -81,6 +91,42 @@ public class Trie {
             cur = cur.son[idx];
         }
         return cur;
+    }
+
+
+    /**
+     * 初始化每个结点的缓存队列
+     */
+    private void initKthMostNodeQueue() {
+        dfs(root);
+    }
+
+    /**
+     * 利用后续遍历，初始化每个结点的缓存队列
+     * @param root
+     * @return
+     */
+    private PriorityQueue<Node> dfs(Node root) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(((o1, o2) -> o1.freq - o2.freq));
+        for (Node next : root.son) {
+            if (next == null) {
+                continue;
+            }
+            PriorityQueue<Node> subPq = dfs(next);
+            // count sub result
+            for (Node node : subPq) {
+                pq.offer(node);
+            }
+        }
+        if (root.isEnd) {
+            pq.offer(root);
+        }
+        while (pq.size() > K) {
+            pq.poll();
+        }
+        // save current cache
+        root.setKthMostNodeQueue(pq);
+        return pq;
     }
 
 
@@ -112,7 +158,7 @@ public class Trie {
             this.isEnd = false;
             this.freq = 0;
             this.endWord = "";
-            this.kthMostNodeQueue = new PriorityQueue<>(((o1, o2) -> o2.freq - o1.freq));
+            this.kthMostNodeQueue = new PriorityQueue<>(((o1, o2) -> o1.freq - o2.freq));
         }
 
         public boolean isEnd() {
